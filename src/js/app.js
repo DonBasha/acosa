06b7f5d20115860a5d359b8e29adc9abbf192aca
Fitim X Kransiqi <fitimkrasniqi@odysseyprime.nl>
2022-03-02 19:42:43 +0100

    added contact form, added loader, added scroll indicator

--- src/js/app.js
+++ src/js/app.js
@@ -3,3 +3,14 @@ require('../scss/style.scss');
 
 /********** Paste your code here! ************/
 
+$(window).on("scroll",()=>{
+  const scroll=$(window).scrollTop()
+  if(scroll>0){
+    $("header").addClass("iScrolling")
+  }else{
+    $("header").removeClass("iScrolling")
+  }
+})
+$(window).on("load",()=>{
+  $("#loader").fadeOut()
+})
